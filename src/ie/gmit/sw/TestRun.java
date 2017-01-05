9a5c5eb4199175384abd37427d4b0f3fb130cef5
paddygriffin <paddygriffin1995@gmail.com>
2017-01-05 13:25:57 +0000

    using reflection to output from jar file

---
+++ src/ie/gmit/sw/TestRun.java
@@ -0,0 +1,16 @@
+package ie.gmit.sw;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class TestRun {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		// TODO Auto-generated method stub
+		Reader reader  = new Reader();
+		reader.getJar("C:/Users/paddy/Desktop/G00314635/string-service.jar");
+		//reader.getJar("C:/Users/paddy/Desktop/G00314635/string-service");
+
+	}
+
+}
